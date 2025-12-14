7a007c897665b9bccf17976b973c0df8048a852d
Alejandro Sampablo <alejandro200304@gmail.com>
2025-12-14 14:21:40 -0600

    Fix Imagenes 2

--- config/urls.py
+++ config/urls.py
@@ -52,6 +52,8 @@ urlpatterns = [
     path('ckeditor/', include('ckeditor_uploader.urls')),
 ]
 
+# Servir archivos media en desarrollo y producción
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
